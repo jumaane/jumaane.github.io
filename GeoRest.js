23580fb20cf49e022ca5569df64acc31b5035fd0
jumaane <jumesyn708@yahoo.com>
2016-11-14 00:42:27 +0900

    Add files via upload

---
+++ GeoRest.js
@@ -0,0 +1,292 @@
+var restaurantCollection = { "type": "FeatureCollection",
+    "features": [
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.655314,139.700995]},
+			"properties": {
+				"name": "Mario's",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6354,139.70395]},
+			"properties": {
+				"name": "Bikkuri Ramen",
+				"genre": [ "Japanese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6334,139.7033]},
+			"properties": {
+				"name": "Fusion Haven",
+				"genre": [ "Japanese", "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6364,139.7695]},
+			"properties": {
+				"name": "Taro",
+				"genre": [ "Japanese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6614,139.7695]},
+			"properties": {
+				"name": "Kung Pow!",
+				"genre": [ "Chinese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.674,139.70345]},
+			"properties": {
+				"name": "Hacienda del Fonda",
+				"genre": [ "Mexican" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.63140, 139.7039]},
+			"properties": {
+				"name": "Jiro II",
+				"genre": [ "Japanese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.664,139.7595]},
+			"properties": {
+				"name": "Palermo Pizza",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.66,139.7395]},
+			"properties": {
+				"name": "Ileana's",
+				"genre": [ "Mexican" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.65554,139.7055]},
+			"properties": {
+				"name": "Luigi's",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.677,139.767]},
+			"properties": {
+				"name": "Bikkuri Gyoza",
+				"genre": [ "Japanese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6677,139.77]},
+			"properties": {
+				"name": "Frijole Pasta",
+				"genre": [ "Mexican", "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6733,139.73395]},
+			"properties": {
+				"name": "Taro",
+				"genre": [ "Japanese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6654,139.7635]},
+			"properties": {
+				"name": "General Tso",
+				"genre": [ "Chinese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.874,139.7012345]},
+			"properties": {
+				"name": "Pollos con Carne",
+				"genre": [ "Mexican" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6114140, 139.703339]},
+			"properties": {
+				"name": "Jiro II",
+				"genre": [ "Japanese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.62164,139.7224445]},
+			"properties": {
+				"name": "Palermo Pizza",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6216,1449.745595]},
+			"properties": {
+				"name": "Ileana's",
+				"genre": [ "Mexican" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.673223,139.7333395]},
+			"properties": {
+				"name": "Taro",
+				"genre": [ "Japanese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.661154,139.765]},
+			"properties": {
+				"name": "General Tso",
+				"genre": [ "Chinese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.874,139.7033345]},
+			"properties": {
+				"name": "Pollos con Carne",
+				"genre": [ "Mexican" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6411140, 139.7039]},
+			"properties": {
+				"name": "Jiro II",
+				"genre": [ "Japanese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.664,139.74555]},
+			"properties": {
+				"name": "Palermo Pizza",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.611116,1449.9795]},
+			"properties": {
+				"name": "Ileana's",
+				"genre": [ "Mexican" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.672313,139.75555]},
+			"properties": {
+				"name": "Taro",
+				"genre": [ "Japanese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.64462254,139.9765]},
+			"properties": {
+				"name": "General Tso",
+				"genre": [ "Chinese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.8244274,139.709345]},
+			"properties": {
+				"name": "Pollos con Carne",
+				"genre": [ "Mexican" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.642230, 139.777]},
+			"properties": {
+				"name": "Jiro II",
+				"genre": [ "Japanese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.8444222,139.94]},
+			"properties": {
+				"name": "Palermo Pizza",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.29,1449.7995]},
+			"properties": {
+				"name": "Ileana's",
+				"genre": [ "Mexican" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.196733,139.73395]},
+			"properties": {
+				"name": "Taro",
+				"genre": [ "Japanese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.699654,139.7695]},
+			"properties": {
+				"name": "Kung Fung Garden",
+				"genre": [ "Chinese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.874,139.970345]},
+			"properties": {
+				"name": "Pollos con Carne",
+				"genre": [ "Mexican" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.645220, 139.70439]},
+			"properties": {
+				"name": "Jiro III",
+				"genre": [ "Japanese" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.6622,139.74445]},
+			"properties": {
+				"name": "Roma Pizza",
+				"genre": [ "Italian" ]
+			}
+		},
+		{
+			"type": "Feature",
+			"geometry": {"type": "Point", "coordinates": [35.62236,1449.74495]},
+			"properties": {
+				"name": "Oleana's",
+				"genre": [ "Mexican" ]
+			}
+		}
+	]
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# jumaane.github.io
+# geobusiness
+Test for Google Maps for Business
+
+Notes:
+- Google Directions API doesn't support TRANSIT
+- Wasn't able to utilize PostGIS (hence couldn't support visits)
+- Unsure of offline availability, would use Local Storage if not already using a local GeoJSON object
